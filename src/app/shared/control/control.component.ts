33f83bc8065398721de0272d23854bac2a2b868f
Wesley de Carvalho Augusto Correia <wcacorreia1995@gmail.com>
2025-03-22 23:12:40 +0000

    modified:   src/app/dashboard/server-status/server-status.component.html 	modified:   src/app/dashboard/server-status/server-status.component.ts 	modified:   src/app/shared/control/control.component.ts

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, ViewEncapsulation } from '@angular/core';
+import { Component, input, ViewEncapsulation, ElementRef, inject } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -8,10 +8,22 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   styleUrl: './control.component.css',
   encapsulation: ViewEncapsulation.None,
   host: {
-    class: 'control'
+    class: 'control',
+    '(click)': 'onClick()'
   }
 })
 export class ControlComponent {
+  // // @HostBinding('class') className = 'control';
+  // @HostListener ('click') onClick() {
+  //   console.log('Clicked!');
+  // }
   label = input.required<string>();
+  private el = inject(ElementRef);
+
+
+  onClick() {
+      console.log('Clicked!');
+      console.log(this.el);
+    }
 
 }
